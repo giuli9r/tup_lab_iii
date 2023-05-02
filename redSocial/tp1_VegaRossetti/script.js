09f7c80b3472ba6270308ef715bd95493eca2d64
giuli9r <giuli.rossetti9@gmail.com>
2023-05-01 21:01:16 -0300

    interaccion btn seguir

---
+++ redSocial/tp1_VegaRossetti/script.js
@@ -0,0 +1,24 @@
+console.log("loaded");
+
+// FUNCIONES
+let followUser = function () {
+
+    if( btnSeguir.textContent == "Seguir") {
+        btnSeguir.textContent = "Dejar de seguir";
+        btnSeguir.className = "unfollow";
+    }
+    else {
+        btnSeguir.textContent = "Seguir";
+        btnSeguir.className = "following";
+    }
+    console.log("siguiendo...");
+}
+
+
+// btnSeguir
+let btnSeguir = document.getElementById("btnSeguir");
+btnSeguir.addEventListener('click', followUser);
+
+
+
+
