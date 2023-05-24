064ddba753e89c294a423b89cbbf432a18e42c8e
xgiuli <grossetti@nexosoluciones.com.ar>
2023-05-24 19:31:23 -0300

    algo de Promesas

--- apis/script.js
+++ apis/script.js
@@ -1,9 +1,34 @@
+console.log("Script 1 cargado.");
 
 function cargarJson() {
     fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => response.json())
     .then (jsonCargado);
 }
+
+function cargarJsonPromise() {
+
+    let promesa = new Promise (resolver => {
+        setTimeout(() => {
+            
+            let obj =
+            {
+                "userId": 1,
+                "id": 1,
+                "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            };
+            
+            resolver(obj);
+        }, 2000);
+
+        });
+
+        promesa.then( json => {
+            jsonCargado(json)
+        });
+}
+
     
 let jsonCargado = function (json) {
     console.log(json);
@@ -16,4 +41,5 @@ let jsonCargado = function (json) {
     user.innerText = json.userId;
 }
 
-cargarJson();
+// cargarJson();
+cargarJsonPromise();
