3d12d18ea1a7512b97810718e9b660dcbf841ce7
giuli9r <giuli.rossetti9@gmail.com>
2023-06-12 21:15:21 -0300

    avanzar con vue

--- autoevaluacion/main.js
+++ autoevaluacion/main.js
@@ -2,14 +2,17 @@
 const app = Vue.createApp ({
     data () {
         return {
+            colorBG : "white",
+            statusCode: 1,
+            mensaje1: " ← ←",
+            mensaje2: " Clicks gratis! Sigue asi... ",
+            mensaje3: " $ 10 / Click",
             alerta: "Descargado correctamente!",
             estudiante: true,
             nombre_alumno: "Giuliano Rossetti",
             nacionalidad: "Argentino" ,
             info_contacto: ['Calle Iturraspe 20XX, 2400, San Francisco Córdoba', 'giuli.rossetti9@gmail.com', '3564 100000' ],
-            perfil: `Estudiante buscando enriquecer los conocimientos técnicos 
-            Tengo la curiosidad para aprender de mucha fuentes.
-            Me interesa aprender desarrollo back-end y Ciencia de Datos`,
+            perfil: `Buscando un desarrollo profesional en el backend.`,
             educacion: [ { id: 1 , institucion: "Universidad Nacional de Córdoba - FCEFyN", enCurso: false, carrera: "Ingenieria Civil" }, 
                         { id: 2 , institucion: "Universidad Nacional de Córdoba - FAMAF", enCurso: false, carrera: "Analista en Sistemas"} , 
                         { id: 3 , institucion: "Universidad Técnologica Nacional - FRSFco", enCurso: true, carrera: "Tecnicatura en Programación"} ]
@@ -18,6 +21,18 @@ const app = Vue.createApp ({
     methods: {
         alertFn (){
             alert(this.alerta);
-        }
+        },
+        pressBtn (){
+            this.statusCode += 1;
+            console.log("Pressed");
+        },
+        updateThis () {
+            this.colorBG= "grey";
+        },
+        updateWhite () {
+            this.colorBG= "white";
+        },
+        
+
     }
 });
